cf197237e0d9dc8847fd74932f8368e065a33bc0
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-12-23 12:58:34 -0500

    First automatically generated .deb package created, woohoo! Add cpack invocation to make deb package

--- ubu/libseawolf/libseawolf/fetchbuild.sh
+++ ubu/libseawolf/libseawolf/fetchbuild.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-echo -e "\nHello, I'm the fetchbuild for $pwd\n"
+git clone -b cmake-and-clang https://github.com/jsalzbergedu/libseawolf.git
+pushd libseawolf
+mkdir build
+pushd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. -GNinja
+ninja
+cpack -GDEB
+popd
+popd

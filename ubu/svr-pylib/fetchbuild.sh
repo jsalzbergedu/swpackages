ac86acfc881b541d3dcb150498ba77ce658cbf5c
Jacob Salzberg <jsalzbergedu@yahoo.com>
2018-02-07 21:10:04 -0500

    Add buildscript for svr-pylib

--- ubu/svr-pylib/fetchbuild.sh
+++ ubu/svr-pylib/fetchbuild.sh
@@ -3,8 +3,8 @@ set -e
 pkgdir="$PWD/svr-pylib"
 mkdir "$pkgdir"
 
-git clone -b cmake-and-clang https://github.com/jsalzbergedu/svr-pylib.git
-qpushd 
+git clone -b packaging https://github.com/jsalzbergedu/svr.git
+qpushd svr
 mkdir build
 qpushd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr .. -GNinja
@@ -12,7 +12,6 @@ ninja
 
 # Going into the python dir
 qpopd
-qpushd src
 qpushd python
 
 # Using setup.py
@@ -21,8 +20,7 @@ python2 setup.py install --root="$pkgdir/" --optimize=1
 
 qpopd
 qpopd
-qpopd
 
 # Finish packaging 
 cp -r DEBIAN "$pkgdir/"
-dpkg -b libseawolf-pylib
+dpkg -b svr-pylib

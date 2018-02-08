d6db3f3150cc03b2672ae67981d4e5da4f5719f0
Jacob Salzberg <jsalzbergedu@yahoo.com>
2018-02-07 20:58:57 -0500

    Add script for svr (but not yet svr-pylib)

--- ubu/svr/fetchbuild.sh
+++ ubu/svr/fetchbuild.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
-
-echo -e "\nHello, I'm the fetchbuild for $pwd\n"
+git clone -b packaging https://github.com/jsalzbergedu/svr.git
+qpushd svr
+mkdir build
+qpushd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. -GNinja
+ninja
+cpack -G DEB
+qpopd
+qpopd
--- ubu/svr/install.sh
+++ ubu/svr/install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "Install file for $PWD"
+qpushd libseawolf
+qpushd build
+
+# You shouldn't use ls in a script,
+# its meant for humans to view not scripts.
+# But dpkg doesnt glob so oh well
+PKGS="$(ls | grep *.deb)"
+
+for pkg in "${PKGS[@]}"
+do
+    sudo dpkg -i "$pkg"
+done
+
+qpopd
+qpopd
